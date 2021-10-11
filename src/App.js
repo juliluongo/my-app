eb4c4601abf95a011d42e6d5ed656737c3dcd0fc
juliluongo <92279200+juliluongo@users.noreply.github.com>
2021-10-11 17:16:21 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React, no seas puto
+          Aprende React
         </a>
       </header>
     </div>
